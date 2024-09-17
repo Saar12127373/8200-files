1923afcfc28566a9460627305ede2acaa546c06b
Saar12127373 <saar7373@gmail.com>
2024-09-17 16:06:40 +0300

    'autosave'

--- socket/client.py
+++ socket/client.py
@@ -14,8 +14,6 @@ def recv_all(length, client_sock):
 
 
 def send_message(message, server_address):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(server_address)
 
     # Convert message to bytes and send length
     message_bytes = message.encode()
@@ -28,15 +26,21 @@ def send_message(message, server_address):
 
 
 
+def recv_response():
     # Receive response
+
     response_len = int.from_bytes(recv_all(4, client_socket), byteorder='big')
     
     response_data = recv_all(response_len, client_socket)
     print(f"Received response: {response_data.decode()}")
+
 
     client_socket.close()
 
 if __name__ == "__main__":
     server_address = ("127.0.0.1", 8543)  # Replace with the server's IP and port
     message = "Hello from the client!"
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(server_address)
+    
     send_message(message, server_address)
