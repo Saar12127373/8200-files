f16b3857ee26c6cca03464e5e3531be12ed24d6c
Saar12127373 <saar7373@gmail.com>
2024-10-23 14:07:01 +0300

    'autosave'

--- weekly_challenge/week4.py
+++ weekly_challenge/week4.py
@@ -5,7 +5,7 @@ def is_sum(s, arr):
     y = len(arr) -1
 
     while(arr[i] < arr[y]): 
-        if(arr[i] + arr[y] == s):
+        if(i + y == s):
             return True
         elif(arr[i] + arr[y] < s):
             i += 1
