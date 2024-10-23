223e3f87dc918b2b71a2e274f9f707e67510fb1d
Saar12127373 <saar7373@gmail.com>
2024-10-23 13:55:12 +0300

    'autosave'

--- weekly_challenge/week4.py
+++ weekly_challenge/week4.py
@@ -1,10 +1,13 @@
 # needs to return if thers a sum taht wil lgive me s
 
 def is_sum(s, arr):
-    pointer1 = arr[0]
-    pointer2 = arr[len(arr)]
+    i = 0
+    y = len(arr)
+    pointer1 = arr[i]
+    pointer2 = arr[y]
     while pointer1 < pointer2:
         if(pointer1 + pointer2 == s):
             return True
         else:
-            pointer1 = arr[]
+            i += 1
+            pointer1 = arr[i]
