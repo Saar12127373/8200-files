f606515b4fc32ec90e0e374c9cc02b0cb69c0dba
Saar12127373 <saar7373@gmail.com>
2024-10-23 13:42:51 +0300

    'autosave'

--- weekly_challenge/week4.py
+++ weekly_challenge/week4.py
@@ -1,5 +1,7 @@
 # needs to return if thers a sum taht wil lgive me s
 
 def is_sum(s, list1):
-    if s > list1[len(list1)/2]:
+    for i in range (list1):
+        if(list1[0] + list1[i] == s):
+            return True
         
