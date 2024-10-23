df5f6ff64956fa8d7406799d8e592b0243ec44db
Saar12127373 <saar7373@gmail.com>
2024-10-23 13:54:21 +0300

    'autosave'

--- weekly_challenge/week4.py
+++ weekly_challenge/week4.py
@@ -4,4 +4,7 @@ def is_sum(s, arr):
     pointer1 = arr[0]
     pointer2 = arr[len(arr)]
     while pointer1 < pointer2:
-        
+        if(pointer1 + pointer2 == s):
+            return True
+        else:
+            pointer1 = arr[]
