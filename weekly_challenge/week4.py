1b6f18f564e7a2d6f28959dad9bb92157e52c84b
Saar12127373 <saar7373@gmail.com>
2024-10-23 13:52:52 +0300

    'autosave'

--- weekly_challenge/week4.py
+++ weekly_challenge/week4.py
@@ -1,7 +1,6 @@
 # needs to return if thers a sum taht wil lgive me s
 
-def is_sum(s, list1):
-    for i in range (list1):
-        if(list1[0] + list1[i] == s):
-            return True
-        
+def is_sum(s, arr):
+    pointer1 = arr[0]
+    pointer2 = arr[len(arr)]
+    
